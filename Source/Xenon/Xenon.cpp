7c8a975ceabe17e8ec53ebff0179acb82e0ba4a7
Zack Xander <48006644+elemcraft@users.noreply.github.com>
2023-03-30 04:46:32 +0800

    Add files via upload

---
+++ Source/Xenon/Xenon.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Xenon.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Xenon, "Xenon" );
