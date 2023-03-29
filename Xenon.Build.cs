87225ec7552bdfb52bb72e7c0ce5300928c51b67
Zack Xander <48006644+elemcraft@users.noreply.github.com>
2023-03-30 04:42:17 +0800

    Add files via upload

---
+++ Xenon.Build.cs
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class Xenon : ModuleRules
+{
+	public Xenon(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+	
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG" });
+
+		PrivateDependencyModuleNames.AddRange(new string[] {  });
+
+		// Uncomment if you are using Slate UI
+		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+		
+		// Uncomment if you are using online features
+		PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		if (Target.Platform == UnrealTargetPlatform.Android)
+        {
+			PrivateDependencyModuleNames.Add("OnlineSubsystemGooglePlay");
+			PrivateDependencyModuleNames.Add("AndroidAdvertising");
+		}
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
